3acbcacb7443d6f14b8e3585b324b638bbb2e5ca
movm <moritzius007@hotmail.de>
2025-08-19 13:29:14 +0200

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://xgwok08o0ccgo4g4cgcoksc8.services.moritz-waechter.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
